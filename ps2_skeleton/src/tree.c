189a2ccad1c3bfd9572b050808d4ff7ea20d7d63
eliassoren <eliasbs@stud.ntnu.no>
2019-09-09 13:38:10 +0200

    Add init and destroy of tree

--- ps2_skeleton/src/tree.c
+++ ps2_skeleton/src/tree.c
@@ -33,6 +33,9 @@ node_print ( node_t *root, int nesting )
 void
 node_init (node_t *nd, node_index_t type, void *data, uint64_t n_children, ...)
 {
+    malloc(sizeof(nd));
+    nd->data = data;
+    nd->children = n_children;
     nd->type = type;
 }
 
@@ -41,6 +44,7 @@ node_init (node_t *nd, node_index_t type, void *data, uint64_t n_children, ...)
 void
 node_finalize ( node_t *discard )
 {
+    free
 }
 
 
